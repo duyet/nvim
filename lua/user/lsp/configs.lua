c629d21806768b9fd98a5a6aea0c1b91d5e8d8ea
Duyet Le <lvduyet@fossil.com>
2022-07-28 01:17:09 +0700

    chore: update lsp servers

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "rust_analyzer" }
+local servers = { "jsonls", "sumneko_lua", "rust_analyzer", "pylsp", "yamlls", "tsserver", "eslint"  }
 
 lsp_installer.setup({
 	ensure_installed = servers,
