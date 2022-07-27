9c0236ce38f8f1dc3a0908e0c5c40bbe48191894
Duyet Le <lvduyet@fossil.com>
2022-07-28 01:19:03 +0700

    chore: update lsp

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -8,12 +8,20 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local completion = null_ls.builtins.completion
+
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
-    -- diagnostics.flake8
-	},
+  debug = false,
+  sources = {
+    formatting.rustfmt,
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+
+    diagnostics.flake8,
+    diagnostics.eslint,
+    diagnostics.pylint,
+
+    completion.spell,
+  },
 })
